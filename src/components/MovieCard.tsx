5c4375e8481dfabd5382d95da4e4c20f05253dce
CatAvadani <catalinaavadani@gmail.com>
2024-08-13 20:46:31 +0200

    Update react router dom

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,17 +1,14 @@
-
-import { Link } from 'react-router-dom';
 import { ResultItem } from '../data/dataResponse';
 
 import { truncateText } from '../utils/truncateText';
-
 
 type MovieCardProps = {
   movie: ResultItem;
 };
 
 function MovieCard({ movie }: MovieCardProps) {
-
   return (
+    // <Link to={`/movie/${movie.id}`}>
     <div className='w-72 h-full bg-black/5 rounded-sm hover:opacity-70 transition-all'>
       <img
         className='w-full h-48 object-cover rounded-t-sm'
@@ -20,11 +17,13 @@ function MovieCard({ movie }: MovieCardProps) {
       />
       <div className='p-4 mt-4 flex flex-col gap-8'>
         <h2 className='text-xl font-bold text-white'>{movie.title}</h2>
-        <p className='text-white/50'>{truncateText(movie.overview, 10)} </p>
-        <p className=' text-amber-500 self-end'>Read more</p>
-
+        <p className='text-white/50'>
+          {truncateText(movie.overview, 10)}{' '}
+          <span className=' text-amber-400'>Read more &gt;&gt;</span>{' '}
+        </p>
       </div>
     </div>
+    // </Link>
   );
 }
 
